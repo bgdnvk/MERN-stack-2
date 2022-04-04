f64c22cdbfca697d649c39d3f4c21827c0440dd9
bgdnvk <bnovykov@gmail.com>
2022-04-05 01:53:08 +0200

    post route

--- controllers/items.js
+++ controllers/items.js
@@ -5,7 +5,21 @@ const itemsRouter = Router()
 
 itemsRouter.get('/', async (req, res) => {
     const items = await Item.find({})
-    res.json(items)
+    res.json(items).end()
+})
+
+itemsRouter.post('/', async (req, res) => {
+    const body = req.body
+
+    if(!body.description) return res.status(400).end()
+
+    const itemObj = new Item({
+        description: body.description,
+        likes: body.likes || 0
+    })
+
+    const savedItem = await itemObj.save()
+    res.status(201).json(savedItem).end()
 })
 
 export { itemsRouter }
