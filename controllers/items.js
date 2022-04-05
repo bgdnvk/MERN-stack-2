3b44d9f595b071316f73a469950909ad11412bef
bgdnvk <bnovykov@gmail.com>
2022-04-05 02:18:04 +0200

    REST api finished

--- controllers/items.js
+++ controllers/items.js
@@ -5,7 +5,17 @@ const itemsRouter = Router()
 
 itemsRouter.get('/', async (req, res) => {
     const items = await Item.find({})
-    res.json(items).end()
+    res.status(200).json(items).end()
+})
+
+itemsRouter.get('/:id', async (req, res) => {
+    const item = await Item.findById(req.params.id)
+
+    if(item){
+        res.status(200).json(item).end()
+    } else{
+        res.status(404).end
+    }
 })
 
 itemsRouter.post('/', async (req, res) => {
@@ -20,6 +30,23 @@ itemsRouter.post('/', async (req, res) => {
 
     const savedItem = await itemObj.save()
     res.status(201).json(savedItem).end()
+})
+
+itemsRouter.delete('/:id', async (req, res) => {
+    await Item.findByIdAndRemove(req.params.id)
+    res.status(204).end()
+})
+
+itemsRouter.put('/:id', async (req, res) => {
+    const body = req.body
+
+    const itemObj = {
+        description: body.description,
+        likes: body.likes
+    }
+
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id, itemObj)
+    res.status(201).json(updatedItem).end()
 })
 
 export { itemsRouter }
