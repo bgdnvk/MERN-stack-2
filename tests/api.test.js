fdbaafef27f26f1e8d767223d8a98692b0500e55
bgdnvk <bnovykov@gmail.com>
2022-04-08 00:02:12 +0200

    GET call

---
+++ tests/api.test.js
@@ -0,0 +1,14 @@
+import supertest from 'supertest'
+import { app } from '../app.js'
+//use the supertest object as our API
+const api = supertest(app)
+
+import { Item } from '../models/item'
+
+//run npm test -- -t "GET call"
+//test GET or READ call on localhost:3001/api/items endpoint
+test('GET call', async () => {
+    await api
+        .get('/api/items')
+        .expect(200)
+})
